534bae405f876dc6fe55cf9c2b92c47cbaf55b6c
Reutov <dikovina8@mail.ru>
2017-07-26 17:11:25 +0400

    delete acc from bitrix

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -28,18 +28,21 @@ namespace ConsoleApplication2
                 mail.Submit();
                 Task.Delay(20000).Wait();
                 mail = driver.FindElementByName("find");
-                mail.SendKeys("qwerty@g.ru");
-                mail = driver.FindElementByCssSelector("html.bx-no-touch.bx-no-retina.bx-firefox.bx-boxshadow.bx-borderradius.bx-flexwrap.bx-boxdirection.bx-transition.bx-transform body#bx-admin-prefix div#window_wrapper table.adm-main-wrap tbody tr td.adm-workarea-wrap.adm-workarea-wrap-top div#adm-workarea.adm-workarea.adm-workarea-page form div#adm-filter-tab-wrap-tbl_user_filter.adm-filter-wrap.adm-current-filter table.adm-filter-main-table tbody tr td.adm-filter-main-table-cell div#tbl_user_filter_content.adm-filter-content.adm-filter-content-first div.adm-filter-content-table-wrap table#tbl_user_filter.adm-filter-content-table tbody tr#tbl_user_filter_row_miss-0 td.adm-filter-item-center div.adm-filter-alignment div.adm-filter-box-sizing span.adm-select-wrap select.adm-select option");
-                mail.Click();
+                mail.SendKeys("qwertyyyyyyyy");
+                //mail = driver.FindElementByCssSelector("html.bx-no-touch.bx-no-retina.bx-firefox.bx-boxshadow.bx-borderradius.bx-flexwrap.bx-boxdirection.bx-transition.bx-transform body#bx-admin-prefix div#window_wrapper table.adm-main-wrap tbody tr td.adm-workarea-wrap.adm-workarea-wrap-top div#adm-workarea.adm-workarea.adm-workarea-page form div#adm-filter-tab-wrap-tbl_user_filter.adm-filter-wrap.adm-current-filter table.adm-filter-main-table tbody tr td.adm-filter-main-table-cell div#tbl_user_filter_content.adm-filter-content.adm-filter-content-first div.adm-filter-content-table-wrap table#tbl_user_filter.adm-filter-content-table tbody tr#tbl_user_filter_row_miss-0 td.adm-filter-item-center div.adm-filter-alignment div.adm-filter-box-sizing span.adm-select-wrap select.adm-select option");
+                //mail.Click();
                 mail = driver.FindElementByName("set_filter");
                 mail.Click();
-                mail = driver.FindElementByXPath("/html/body/div[1]/table/tbody/tr[2]/td[2]/div/div[2]/div[1]/form/table/tbody/tr/td[1]/label");
+                Task.Delay(4000).Wait();
+                mail = driver.FindElementByXPath("/html/body/table/tbody/tr[2]/td[2]/div/div[2]/div[1]/form/table/tbody/tr/td[3]/a");
                 mail.Click();
-                mail = driver.FindElementById("action_delete_button");
+                mail = driver.FindElementById("btn_delete");
                 mail.Click();
+                driver.SwitchTo().Window(driver.WindowHandles.ToList().Last());
                 Task.Delay(1000).Wait();
-                mail.Submit();
-
+                //mail.Submit();
+                IAlert alert = driver.SwitchTo().Alert();
+                alert.Accept();
                 Console.ReadKey();
 
 
