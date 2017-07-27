c213c6ab79939b6d3930d6ecfbfee9a10334c036
Reutov <dikovina8@mail.ru>
2017-07-27 10:13:55 +0400

    delete sale and user

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -18,19 +18,30 @@ namespace ConsoleApplication2
             
             using (var driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/bitrix/admin/user_admin.php?lang=ru");
+
+                driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/bitrix/admin/sale_order.php?lang=ru");
                 driver.Manage().Window.Maximize();
                 var mail = driver.FindElementByName("USER_LOGIN");
-                mail.SendKeys("merikanov95@mail.ru");
+                mail.SendKeys("Bot");
                 mail = driver.FindElementByName("USER_PASSWORD");
-                mail.SendKeys("rorshach1994");
-                //catalog = driver.FindElementByName("Login");
+                mail.SendKeys("123456");
                 mail.Submit();
-                Task.Delay(20000).Wait();
+                Task.Delay(10000).Wait();
+                mail = driver.FindElementByName("filter_account_number");
+                mail.SendKeys("113579");
+                mail = driver.FindElementById("tbl_sale_order_filterset_filter");
+                mail.Click();
+                Task.Delay(3000).Wait();
+                mail = driver.FindElementByXPath("/html/body/table/tbody/tr[2]/td[2]/div/div[4]/div[1]/form/table/tbody/tr/td[3]/table/tbody/tr/td[2]/b/a");
+                mail.Click();
+                mail = driver.FindElementById("btn_delete");
+                mail.Click();
+                IAlert alert = driver.SwitchTo().Alert();
+                alert.Accept();
+                driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/bitrix/admin/user_admin.php?lang=ru");
+                Task.Delay(5000).Wait();
                 mail = driver.FindElementByName("find");
-                mail.SendKeys("qwertyyyyyyyy");
-                //mail = driver.FindElementByCssSelector("html.bx-no-touch.bx-no-retina.bx-firefox.bx-boxshadow.bx-borderradius.bx-flexwrap.bx-boxdirection.bx-transition.bx-transform body#bx-admin-prefix div#window_wrapper table.adm-main-wrap tbody tr td.adm-workarea-wrap.adm-workarea-wrap-top div#adm-workarea.adm-workarea.adm-workarea-page form div#adm-filter-tab-wrap-tbl_user_filter.adm-filter-wrap.adm-current-filter table.adm-filter-main-table tbody tr td.adm-filter-main-table-cell div#tbl_user_filter_content.adm-filter-content.adm-filter-content-first div.adm-filter-content-table-wrap table#tbl_user_filter.adm-filter-content-table tbody tr#tbl_user_filter_row_miss-0 td.adm-filter-item-center div.adm-filter-alignment div.adm-filter-box-sizing span.adm-select-wrap select.adm-select option");
-                //mail.Click();
+                mail.SendKeys("qwert@g.ro");
                 mail = driver.FindElementByName("set_filter");
                 mail.Click();
                 Task.Delay(4000).Wait();
@@ -40,8 +51,7 @@ namespace ConsoleApplication2
                 mail.Click();
                 driver.SwitchTo().Window(driver.WindowHandles.ToList().Last());
                 Task.Delay(1000).Wait();
-                //mail.Submit();
-                IAlert alert = driver.SwitchTo().Alert();
+                alert = driver.SwitchTo().Alert();
                 alert.Accept();
                 Console.ReadKey();
 
