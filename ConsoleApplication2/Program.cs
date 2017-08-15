342ce930b266f6494934229bed16b5e6513e30a5
Reutov <dikovina8@mail.ru>
2017-08-15 14:44:42 +0400

    new test

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -8,11 +8,13 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Timers;
 
 namespace ConsoleApplication2
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             
@@ -20,61 +22,52 @@ namespace ConsoleApplication2
             {
 
                 driver.Manage().Window.Maximize();
-                driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/products/digital_av/av_accessories/head_phone/RP-TCM105E/");
+                driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/products/digital_av/av_accessories/head_phone/RP-HT090/");
                 driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
                 var to = driver.FindElementByXPath("/html/body/div[1]/div/div/div/div[3]/div/div/div[2]/div[2]/div[1]/div[2]/table/tbody/tr/td[3]/div/div/span/span/a[1]");
                 to.Click();
 
                 Task.Delay(2000).Wait();
-                driver.SwitchTo().Window(driver.WindowHandles.ToList().Last());
+                
 
-                var popap = driver.FindElementById("cboxClose");
-                popap.Click();
+                var quantity = driver.FindElementByXPath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[2]/div[3]/div/span[2]");
+                quantity.Click();
+                quantity.Click();
                 Task.Delay(2000).Wait();
                 to = driver.FindElementByXPath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[4]/a");
                 to.Click();
-                var registration = driver.FindElementByName("NEW_EMAIL");
-
-                string s = "";
-                Random rand = new Random();
-                for (int i = 0; i < 6; i++)
-                {
-                    s += Convert.ToChar(rand.Next(65, 90));
-                }
-
-                registration.SendKeys(s + "@mail.ru");
+                var auth = driver.FindElementByXPath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/form/div[1]/div/div[1]/div[2]/div/a[2]");
+                auth.Click();
+                auth = driver.FindElementByName("USER_LOGIN");
+                auth.SendKeys("botadmin@p33.org");
+                auth = driver.FindElementByName("USER_PASSWORD");
+                auth.SendKeys("123456");
+                auth = driver.FindElementById("js_auth_button");
+                auth.Click();
+                Task.Delay(10000).Wait();
+                var city = driver.FindElementByXPath("/html/body/div[3]/div/div/div/div[3]/div/div[2]/form/div[1]/div/div[1]/div[2]/div[2]/div/a/span");
+                city.Click();
                 driver.SwitchTo().Window(driver.WindowHandles.ToList().Last());
 
-                var city = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[2]/div[2]/div[2]/div/a/span");
+                //to = driver.FindElementByXPath("/html/body/div[3]/div/div/div/div[3]/div/div[2]/form/div[1]/div/div[1]/div[2]/div[2]/div/a/span");
+                //to.Click();
+
+
+
+
+                //driver.SwitchTo().Window(driver.WindowHandles.ToList().Last());
+                Task.Delay(3000).Wait();
+                //driver.SwitchTo().Window(driver.WindowHandles.ToList().Last());
+                city = driver.FindElementByXPath("/html/body/div[11]/div[1]/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/ul/li[1]/a");
                 city.Click();
                 Task.Delay(2000).Wait();
-                var city2 = driver.FindElementByXPath("/html/body/div[3]/div[1]/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/ul/li[1]/a");
-                city2.Click();
-                Task.Delay(2000).Wait();
-                var adress = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[2]/div[4]/div[2]/input");
-                adress.SendKeys("Тестовая");
-                Task.Delay(500).Wait();
-                var index = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[2]/div[5]/div[2]/input");
-                index.SendKeys("123456");
-                Task.Delay(500).Wait();
-                var password = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[3]/div[3]/div[2]/input");
-                password.SendKeys("Cent73");
-                Task.Delay(500).Wait();
-                var password2 = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[3]/div[4]/div[2]/input");
-                password2.SendKeys("Cent73");
-                Task.Delay(500).Wait();
-                var name = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[3]/div[5]/div[2]/input");
-                name.SendKeys("Тест");
-                Task.Delay(500).Wait();
-                var famali = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[3]/div[6]/div[2]/input");
-                famali.SendKeys("Тестов");
-                Task.Delay(500).Wait();
-                var namber = driver.FindElementByXPath("//*[@id=\"process_order\"]/div[1]/div/div[3]/div[7]/div[2]/input");
-                namber.SendKeys("92933087899");
-                Task.Delay(500).Wait();
-                var pol = driver.FindElementByXPath("/html/body/div[1]/div/div/div/div[3]/div/div[2]/form/div[1]/div/div[3]/div[8]/div[2]/div/div/select/option[2]");
-                pol.Click();
-                pol.Submit();
+                to = driver.FindElementByName("ORDER_PROP_5");
+                to.SendKeys("Testing");
+                to = driver.FindElementByName("ORDER_PROP_4");
+                to.SendKeys("111111");
+                //to = driver.FindElementByXPath("/html/body/div[3]/div/div/div/div[3]/div/div[2]/form/div[1]/div/div[8]/a");
+                to.Submit();
+
                 driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/bitrix/admin/sale_order.php?lang=ru");
                 Task.Delay(1000).Wait();
                 var mail = driver.FindElementByName("USER_LOGIN");
@@ -85,7 +78,7 @@ namespace ConsoleApplication2
                 mail.Submit();
                 Task.Delay(10000).Wait();
                 var nmail = driver.FindElementByName("filter_user_login");
-                nmail.SendKeys(s + "@mail.ru");
+                nmail.SendKeys("@mail.ru");
                 mail = driver.FindElementById("tbl_sale_order_filterset_filter");
                 mail.Click();
                 Task.Delay(5000).Wait();
@@ -100,7 +93,7 @@ namespace ConsoleApplication2
                 driver.Navigate().GoToUrl("https://eplaza.panasonic.ru/bitrix/admin/user_admin.php?lang=ru");
                 Task.Delay(5000).Wait();
                 mail = driver.FindElementByName("find");
-                mail.SendKeys(s + "@mail.ru");
+                mail.SendKeys("@mail.ru");
                 mail = driver.FindElementByName("set_filter");
                 mail.Click();
                 Task.Delay(4000).Wait();
@@ -113,9 +106,6 @@ namespace ConsoleApplication2
                 alert = driver.SwitchTo().Alert();
                 alert.Accept();
                 Console.ReadKey();
-
-
-
 
             }
         }
